7d7f452f833fdc7aebfb9d6118e0c59457b081e1
KevinJSS <kevin.alvarado@ucrso.info>
2023-05-11 22:43:15 -0600

    Unit testing failed example

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 module.exports = {
-
     helloWorld: function() {
         return 'Hello world';
     },
 
     sum: function(num1, num2) {
-        return num1 + num2; 
+        return num1 - num2; 
     }
-
-
-
 }
